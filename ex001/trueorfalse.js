df5ae49430b50a499ebe07ab4c4cf5b4847de4d0
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-10-12 20:30:30 -0300

    comprando strings

--- ex001/trueorfalse.js
+++ ex001/trueorfalse.js
@@ -5,3 +5,9 @@ const primeiroNumero = 11;
 const segundoNumero = 11;
 
 console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "olá"
+
+const text2 = "olá"
+
+console.log(texto1 === text2);

480db195b47724e3ee4cedf20e773ff0cd9e7624
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-10-12 20:26:00 -0300

    boolean value

---
+++ ex001/letras.js
@@ -0,0 +1,8 @@
+// strings
+
+const texto1 = "Olá, mundo!";
+const texto2 = "Hello wolrd";
+
+const stringComAspas = 'Opa, e Deus disse: "Que haja luz!"';
+
+console.log(stringComAspas);
---
+++ ex001/trueorfalse.js
@@ -0,0 +1,7 @@
+// boolean values
+
+const primeiroNumero = 11;
+
+const segundoNumero = 11;
+
+console.log(primeiroNumero === segundoNumero);

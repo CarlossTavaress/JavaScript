a3849f62c3aa4aa05e4c9b8db0a87a1791f0ea8c
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-10-12 19:49:15 -0300

    getting rid of the curse

--- ex001/numeros.js
+++ ex001/numeros.js
@@ -1,3 +1,11 @@
+const primeiroComando = 'Olá, Mundo!';
+
+console.log(primeiroComando);
+
+const segundoComando = 'Hello, wolrd!';
+
+console.log(segundoComando);
+
 const nunNumero = 13;
 
 const primeiroNumero = 1;
@@ -6,3 +14,17 @@ const segundoNumero = 2;
 const somaNum = primeiroNumero + segundoNumero;
 
 console.log(somaNum);
+
+// pontos flutuantes 
+
+const numFlutuante = 9.9;
+
+const numFlut = .3;
+
+const somaFlut = numFlut + numFlutuante;
+
+console.log(somaFlut);
+
+const somaSomaFlut = numFlut + nunNumero;
+
+console.log(somaSomaFlut);

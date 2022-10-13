9b49e0528a7601e1db963a2ce8f80119c4e6c9b5
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-10-13 15:34:02 -0300

    variaveis tipo var let e const

---
+++ ex002/tiposvariaveis.js
@@ -0,0 +1,43 @@
+// var  
+
+var altura =  9;
+var comprimento = 10;
+
+area = altura * comprimento;
+
+//console.log(area);
+
+// let
+
+/*let forma = 'retângulo'
+let altura2 = 11;
+let comprimento2 = 11;
+let area2;
+
+
+if(forma === 'retângulo'){
+    area2 = (altura2 * comprimento2);
+}else {
+    area2 = (altura2 * comprimento2) / 2;
+} 
+
+console.log(area2); */
+
+// const
+
+const forma1 = "quadrado"
+const altura3 = 5;
+const comprimento3 = 7;
+
+
+if (forma1 === "quadrado"){
+     area3 = altura3 * comprimento3;
+}else {
+     area3 = (altura3 * comprimento3) / 2;
+}
+
+
+console.log(area3);
+
+
+

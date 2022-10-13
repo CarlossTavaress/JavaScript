bc68ac1921a59e07292f81641b6678385fe41fe0
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-10-13 15:52:00 -0300

    truthy falsy null undefined

---
+++ ex002/trueorfalsee.js
@@ -0,0 +1,17 @@
+// boolean 
+
+const usuarioLogado = true;
+const contaVencida = false;
+
+// truthy ou falsy
+
+/* console.log("" == false);
+console.log(1 == true); */
+
+// null & undefined 
+
+let minhaVar = "HeheXD";
+let varUndefined = "Obrigado meu Deus!"
+
+console.log(minhaVar);
+console.log(varUndefined);

c47d64de1e823e45663fe9a856695587da086b14
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-10-14 16:00:29 -0300

    finalizando

---
+++ ex003/console.js
@@ -0,0 +1,14 @@
+//console.api
+
+/*
+
+const minhaVar = true;
+
+console.log(100);
+console.log("Hello, this is a text");
+console.log(minhaVar); */
+
+//tratamento de erro!
+
+//console.error('Opa, deu erro hehe');
+

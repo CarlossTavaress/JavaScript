79a044fa01ca80eff3f9a994dc00ab95714eb965
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-10-15 15:46:20 -0300

    operadorternario.js

---
+++ ex004/operadorternario.js
@@ -0,0 +1,12 @@
+// operador ternario
+
+const idadeMinima = 18;
+const idadeCliente = 18;
+/*
+if(idadeCliente >= idadeMinima){
+    console.log("Você está autorizado a comprar bebida alcoolica.")
+}else {
+    console.log("Proibida a compra de bebida.")
+}*/
+
+console.log(idadeCliente >= idadeMinima ? "autorizado a comprar bebida" : "não autorizado a comprar bebida");

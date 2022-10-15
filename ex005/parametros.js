0e3bd63991a861b87f42f12874e5f3485bef99fe
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-10-15 16:56:58 -0300

    finalizando os argumentos e funções

---
+++ ex005/parametros.js
@@ -0,0 +1,28 @@
+/* function soma(num1, num2){
+    return num1 + num2;
+}
+
+console.log(soma(3, 5));
+console.log(soma(10, 15));
+console.log(soma(-35, 509));
+console.log(soma(8790, 13203));
+
+
+function minhaApresentacao(nome, idade){
+    return `Meu nome e ${nome} e tenho ${idade} anos.`
+}
+
+console.log(minhaApresentacao("Doctor of Love", 25));
+
+
+*/
+
+function soma(num1, num2){
+    return num1 + num2;
+}
+
+function multiplicaNum(numero1 = 1, numero2 = 1){
+    return numero1 * numero2;
+}
+
+console.log(multiplicaNum(soma(4,5), ));

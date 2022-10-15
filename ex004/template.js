515fa39313a0bb1c8bff8b95fdaafe34651e27cd
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-10-15 16:04:52 -0300

    template stringss

--- ex004/template.js
+++ ex004/template.js
@@ -1,2 +1,8 @@
 //template strings
 
+const nome = " Doctor of Love";
+const idade = 2022 - 1997;
+const cidadeDeNascimento = "Belém"
+
+const apresentacao = `Meu nomé é ${nome}, tenho ${idade} anos e nasci na cidade de  ${cidadeDeNascimento}.`;    
+console.log(apresentacao)

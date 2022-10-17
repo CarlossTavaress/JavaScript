afdf3b875ae981f2823ba4bd2c357408d8fac096
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-10-17 17:58:56 -0300

    criação do primeiro array em js

---
+++ ex006/arrayp01.js
@@ -0,0 +1,18 @@
+// medias : 10, 6, 5, 7.5, 8
+
+let nota1 = 10;
+let nota2 = 6;
+let nota3 = 5;
+let nota4 = 7.5;
+let nota5 = 8;
+
+
+let media = (nota1 + nota2 + nota3 + nota4 + nota5)/ 5;
+
+// console.log(`A média das notas é: ${media}`);
+
+const notas = [10, 9, 7, 9.5, 5];
+
+let media1 = (notas[0] + notas[1] + notas[2] + notas[3] + notas[4]) / notas.length;
+
+console.log(`Média da turma é: ${media1}`);

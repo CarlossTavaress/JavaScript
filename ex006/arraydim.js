cf666d76d4292fcf4394b462a33dd029631d9147
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-10-18 10:07:22 -0300

    lista com 2 dimensões

---
+++ ex006/arraydim.js
@@ -0,0 +1,6 @@
+const alunos = ["João", "Juliana", "Barbara", "Ana"];
+const mediaAlunos = [10, 7, 9, 6.5];
+
+let listaDeNotasEAlunos = [alunos, mediaAlunos];
+
+console.log(`Nome dos alunos ${listaDeNotasEAlunos[0]} Média dos alunos: ${listaDeNotasEAlunos[1]}.`);

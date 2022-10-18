4138ed18c85b4fde6be5a78fac45b41d7315bc3c
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-10-18 10:07:58 -0300

    continue

--- ex006/arraydim.js
+++ ex006/arraydim.js
@@ -3,4 +3,4 @@ const mediaAlunos = [10, 7, 9, 6.5];
 
 let listaDeNotasEAlunos = [alunos, mediaAlunos];
 
-console.log(`Nome dos alunos ${listaDeNotasEAlunos[0]} Média dos alunos: ${listaDeNotasEAlunos[1]}.`);
+console.log(`Nome dos alunos: ${listaDeNotasEAlunos[0]} Média dos alunos: ${listaDeNotasEAlunos[1]}.`);

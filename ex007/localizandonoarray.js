bf1384a0b0b6745d1845325db1df0a8115a155a0
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-10-19 18:35:23 -0300

    aprendendo

--- ex007/localizandonoarray.js
+++ ex007/localizandonoarray.js
@@ -3,13 +3,15 @@ const notas = [9.6, 10, 8, 7.9];
 
 let listaNotasEAlunos = [alunos, notas];
 
-const exibeNomeNota = (nomeDoAluno) => {
-    if(listaNotasEAlunos[0].includes(nomeDoAluno)){
-        indice = listaNotasEAlunos[0].indexOf(nomeDoAluno)
-        return listaNotasEAlunos[0][indice] + 'sua média é: ' + listaNotasEAlunos[1][indice]
-    } else {
+const exibeAluno = (nomeAluno) => {
+    if(listaNotasEAlunos[0].includes(nomeAluno)){
+        indice = listaNotasEAlunos[0].indexOf(nomeAluno)
+        return listaNotasEAlunos[0][indice] + ' sua nota é: ' + listaNotasEAlunos[1][indice]
+    }else {
         return 'Aluno não encontrado'
     }
 }
 
-console.log(exibeNomeNota("Ana"));
+console.log(exibeAluno("Ana"))
+
+console.log(listaNotasEAlunos[0][2] + listaNotasEAlunos[1][3]);

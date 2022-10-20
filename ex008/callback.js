a24ea1670c831a9bc6706a706b86c605ce14c053
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-10-20 19:29:00 -0300

    metodo callback

---
+++ ex008/callback.js
@@ -0,0 +1,10 @@
+const times = ["Internacional", "Flamengo", "Corinthians", "Vasco", "São Paulo", "Palmeiras", "Santos"]
+const selecoes = ["Brasil", "Alemanha", "Argentina", "Inglaterra", "França"]
+
+function imprimeArray(time){
+    console.log(time)
+}
+
+times.forEach(imprimeArray)
+console.log("------------------")
+selecoes.forEach(imprimeArray)

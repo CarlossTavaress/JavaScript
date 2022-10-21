c8977e915547633617178f51a84edd7baf5c7b2a
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-10-21 18:11:17 -0300

    ex reduce

--- ex009/reduce.js
+++ ex009/reduce.js
@@ -2,3 +2,9 @@ const salaJS = [7,8,1,2,10,5,6,10,9];
 const salaJava = [6,5,8,9,5,6];
 const salaPython = [7, 3.5, 8, 9.5];
 
+function mediaSala(salaProg){
+    const somaDasMedias = salaProg.reduce((acumulador, atual) => acumulador + atual,0)
+    return somaDasMedias / salaProg.length
+}
+
+console.log(mediaSala(salaJS));

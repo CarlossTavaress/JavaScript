9f5e2d50ad7f14065fd18ec55363e62744a14df6
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-10-24 19:11:26 -0300

    imprimindo o objeto

---
+++ ex010/acess.js
@@ -0,0 +1,8 @@
+const cliente = {
+    nome : "DrLove",
+    idade : 25,
+    cpf : "1923881241129",
+    email: "drlove@contato.com"
+}
+
+console.log(`Nome: ${cliente.nome} - Idade: ${cliente.idade}`);

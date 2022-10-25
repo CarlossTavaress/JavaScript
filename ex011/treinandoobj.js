21450a6998e71d5e342afc0d07d1b6ad31a93f71
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-10-25 18:20:31 -0300

    exercício fixação

---
+++ ex011/treinandoobj.js
@@ -0,0 +1,19 @@
+const time = {
+    nome: "Internacional",
+    ano: 1909,
+    estado: "Rio Grande do Sul",
+    titulo: "Campeão Mundial"
+}
+
+console.log(time)
+
+time.titulosSeculo21 = {
+    nacionais : "Copa do Brasil/Gauchão",
+    continental : "Libertadores 2X"
+}
+
+console.log(time)
+
+time.titulosSeculo21.brasileirao = "Campeão Brasileiro";
+
+console.log(time)

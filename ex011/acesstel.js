59cb682a5366c849205bfa957daeaa905ecdfcc5
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-10-25 18:06:35 -0300

    array no objeto

---
+++ ex011/acesstel.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome : "DrLove",
+    idade : 25,
+    cpf : "1923881241129",
+    email: "drlove@contato.com",
+    fones: ["55592883", "92139810", "0391284128"]
+}
+
+cliente.fones.forEach(fone => console.log(fone))

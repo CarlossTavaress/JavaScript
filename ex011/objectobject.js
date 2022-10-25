a4db3a360634ee01ffd30d38335b24408c9418aa
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-10-25 18:15:34 -0300

    alterando obj dentro do obj

---
+++ ex011/objectobject.js
@@ -0,0 +1,19 @@
+const cliente = {
+    nome : "DrLove",
+    idade : 25,
+    cpf : "1923881241129",
+    email: "drlove@contato.com",
+    fones: ["55592883", "92139810", "0391284128"]
+}
+
+cliente.dependete = {
+    nome: "Francheskkah",
+    idade: 8,
+    parentesco: "Gata"
+}
+
+console.log(cliente)
+
+cliente.dependete.nome = "Francheskkah Hyuga"
+
+console.log(cliente)

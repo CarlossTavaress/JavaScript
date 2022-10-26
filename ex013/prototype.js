a1c8c80966e5821847c542346969598cffe162a0
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-10-26 17:28:10 -0300

    heranças

--- ex013/prototype.js
+++ ex013/prototype.js
@@ -2,12 +2,4 @@ const arrayComum = [1,2,4,5,19];
 
 const outroArray = new Array([1,2,3,4,5]);
 
-console.log(outroArray);
-console.log(arrayComum);
-let i = 0;
-
-arrayComum.forEach( comum => {
-    i += comum
-})
-
-console.log(i)
+// JavaScript é baseado em prototipo e herda heranças._

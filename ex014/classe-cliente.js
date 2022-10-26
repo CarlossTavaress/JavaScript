4bc8ad6096161f3b9216f7998882081d11060a44
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-10-26 20:18:14 -0300

    class

--- ex014/classe-cliente.js
+++ ex014/classe-cliente.js
@@ -5,4 +5,18 @@ class Cliente{
         this.cpf = cpf
         this.saldo = saldo
     }
+    depositar(x){
+        this.saldo += x
+    }
+    sacar(y){
+        this.saldo -= y
+    }
+    exibirSaldo(){
+        console.log(this.saldo)
+    }
 }
+
+const drLove = new Cliente("DrLove", "drlove@contato", "1023182481241", 907739222);
+
+drLove.depositar(20000)
+drLove.exibirSaldo()

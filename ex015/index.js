bde432c7e63f335fe8aa802f66a39e1f494b7a6e
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-10-27 16:45:25 -0300

    consertando o codigo

--- ex015/index.js
+++ ex015/index.js
@@ -4,15 +4,16 @@ const livros = require('./listaLivros');
 
 function ordenaPosicao(arrProdutos, posicaoInicial){
     let menorPreco = posicaoInicial;
-    for(atual = 0; atual < arrProdutos.length; atual++){
+
+    for(atual = posicaoInicial; atual < arrProdutos.length; atual++){
     if(arrProdutos[atual].preco < arrProdutos[menorPreco].preco){
         menorPreco = atual
+     }
     }
-}
     return menorPreco;
 }
 
 
-console.log(`Menor preço dos livros: R$ ${livros[menorPreco].preco} e o titulo é ${livros[menorPreco].titulo}`)
+//console.log(`Menor preço dos livros: R$ ${livros[menorPreco].preco} e o titulo é ${livros[menorPreco].titulo}`)
 
 module.exports = ordenaPosicao;
--- ex015/selectionSort.js
+++ ex015/selectionSort.js
@@ -1,6 +1,14 @@
-const menorPreco = require('./index');
+const ordenaPosicao = require('./index');
 const livros = require('./listaLivros');
 
 for(let atual = 0; atual < livros.length; atual++){
+    let menor = ordenaPosicao(livros, atual)
     
+    let livroAtual = livros[atual];
+    let livroMenorPreco = livros[menor];
+
+    livros[atual] = livroMenorPreco;
+    livros[menor] = livroAtual;
 }
+
+console.log(livros)

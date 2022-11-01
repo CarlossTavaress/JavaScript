1d0992dc260b6a15d4f517e4ef180b129bcb0174
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-11-01 11:04:18 -0300

    marge sort

---
+++ ex017/index.js
@@ -0,0 +1,10 @@
+const listaLivros = require('./array');
+
+function margeSort(array){
+
+    if(array.length > 1){
+        const meio = Math.floor(array.length / 2);
+        const part1 = array.slice(0,meio);
+        const part2 = array.slice(meio, array.length);
+    }
+}

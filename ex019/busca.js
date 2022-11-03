4f2f9f8dd57daa6fb5a287f84f6efec3fc56e917
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-11-03 18:19:22 -0300

    ajustando as funcoes de busca

--- ex019/busca.js
+++ ex019/busca.js
@@ -1,1 +1,18 @@
 const listaLivros = require('./arrayOrdenado');
+
+function busca(array, de, ate, valorBusca){ // "de" é onde o indice começa e "ate" é onde vai o array, valorbusca é o valor q será buscado
+    const meio = Math.floor((de + ate) / 2);
+    const atual = array[meio];
+
+    if(valorBusca === atual.preco){
+        return meio;
+    }
+
+    if(valorBusca < atual.preco){
+        return busca(array, de, meio - 1, valorBusca)
+    }
+
+    if(valorBusca > atual.preco){
+
+    }
+}

d1cc62deb082b4e4818f93a3bf5b0a6a6c982e36
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-11-08 17:53:08 -0300

    ajustando a function

--- Importacao/ex01/index.js
+++ Importacao/ex01/index.js
@@ -2,16 +2,19 @@ import fs from 'fs';
 
 
 function trataErro(erro){
-    throw
+    throw new Error(erro.code, 'não há arquivo para ser lido no diretório');
 }
  
 function pegaArquivo(caminhoDoArquivo){
     const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+        if (erro){
+            trataErro(erro);
+        }
         console.log(texto);
     })
 }
 
-pegaArquivo('./importacao/ex01/texto.md')
+pegaArquivo('./importacao/ex01/texto1.md')
 
 

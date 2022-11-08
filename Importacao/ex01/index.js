c8ca5e93c58647b4d89499e164c3093c18263d22
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-11-08 18:23:45 -0300

    inicializando o promises

--- Importacao/ex01/index.js
+++ Importacao/ex01/index.js
@@ -4,7 +4,15 @@ import fs from 'fs';
 function trataErro(erro){
     throw new Error(erro.code, 'não há arquivo para ser lido no diretório');
 }
- 
+
+function pegaArquivo(caminhoDoArquivo){
+    const encoding = 'utf-8';
+    fs.promises.readFile(caminhoDoArquivo, encoding)
+    .then((texto) => console.log(texto))
+    .catch((texto) => trataErro(erro))
+}
+
+/*
 function pegaArquivo(caminhoDoArquivo){
     const encoding = 'utf-8';
     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
@@ -13,8 +21,8 @@ function pegaArquivo(caminhoDoArquivo){
         }
         console.log(texto);
     })
-}
+} */
 
-pegaArquivo('./importacao/ex01/texto1.md')
+pegaArquivo('./importacao/ex01/texto.md')
 
 

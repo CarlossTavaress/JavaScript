a680056504022c11daf890fd68c7f3c78e767ba2
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-11-08 18:28:08 -0300

    forma sincrona

--- Importacao/ex01/index.js
+++ Importacao/ex01/index.js
@@ -7,21 +7,18 @@ function trataErro(erro){
 
 function pegaArquivo(caminhoDoArquivo){
     const encoding = 'utf-8';
-    fs.promises.readFile(caminhoDoArquivo, encoding)
-    .then((texto) => console.log(texto))
-    .catch((texto) => trataErro(erro))
+    const texto = fs.promises.readFile(caminhoDoArquivo, encoding)
+    console.log(texto);
 }
 
 /*
 function pegaArquivo(caminhoDoArquivo){
     const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-        if (erro){
-            trataErro(erro);
-        }
-        console.log(texto);
-    })
-} */
+    fs.promises.readFile(caminhoDoArquivo, encoding)
+    .then((texto) => console.log(texto))
+    .catch((texto) => trataErro(erro))
+}
+*/
 
 pegaArquivo('./importacao/ex01/texto.md')
 

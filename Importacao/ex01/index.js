811cbbf2e0ebb691c04cdb2a0fc4fad30c83cf80
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-11-10 18:17:18 -0300

    extraindo apenas os links com o titulo

--- Importacao/ex01/index.js
+++ Importacao/ex01/index.js
@@ -1,7 +1,5 @@
 import fs from 'fs';
 
-const textoTeste = "São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)."
-
 
 function trataErro(erro){
     throw new Error(erro.code, 'Arquivo não encontrado no diretório'); // caso de erro, está função irá retornar o tipo do erro e a mensagem escrita.
@@ -10,9 +8,14 @@ function trataErro(erro){
 
 function extraiLink(texto){
     const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
-    const captura = regex.exec(texto);
+    const captura = [...texto.matchAll(regex)];
     console.log(captura);
+    const resultado = captura.map(captura =>( {[captura[1]] : captura[2]}))
+    console.log(resultado)
 }
+
+
+
 
 extraiLink(textoTeste);
 

fc290ee49521d49f5d469fe5da780e9d485a9063
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-11-11 18:34:18 -0300

    validando links

--- Importacao/src/cli.js
+++ Importacao/src/cli.js
@@ -1,20 +1,32 @@
 import pegaArquivo from "./index.js";
+import validaLinks from "./htpp-validacao.js";
 
 import fs from "fs";
 
 const caminho = process.argv;
 pegaArquivo(caminho[2]);
 
-function imprimeLista(resultado, identificador = ''){
-    console.log((
+function imprimeLista(valida, resultado, identificador = ''){
+
+    if(valida){
+            console.log((
+        'Lista validada'),
+        identificador,
+        validaLinks(resultado));
+    }else{
+            console.log((
         'Lista de links'),
         identificador,
-        resultado);
+        validaLinks(resultado));
+    }
 
 }
 
 
 async function processaTexto(argumentos){
+
+    const caminho = argumentos[2];
+    const valida = argumentos[3] === 'valida';
 
     try{
         fs.lstatSync(caminho);
@@ -27,17 +39,17 @@ async function processaTexto(argumentos){
         }
     }
 
-    const caminho = argumentos[2];
+    
 
     if (fs.lstatSync(caminho).isFile()){
         const resultado =  await pegaArquivo(argumentos[2]);
-        imprimeLista(resultado);
+        imprimeLista(valida, resultado);
     }else if (fs.lstatSync(caminho).isDirectory()){
         const arquivos = await fs.promises.readdir(caminho);
         arquivos.forEach(async (nomeDeArquivo) => {
             const lista = await pegaArquivo(`${caminho}/ ${nomeDeArquivo}`);
         })        
-        imprimeLista(lista, nomeDeArquivo);
+        imprimeLista(valida, lista, nomeDeArquivo);
     }    
 }
 

cdcb36ed80b5872fd83c200249f8a4ffd6d92b2f
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-11-11 18:56:18 -0300

    adicionando um aluno

--- Parte1/ex009/filtrando.js
+++ Parte1/ex009/filtrando.js
@@ -1,5 +1,5 @@
-const alunos = ["Carlos", "Maria", "Julia", "Rogério", "Viviane"];
-const notas = [10, 8, 9, 3, 4];
+const alunos = ["Carlos", "Maria", "Julia", "Rogério", "Viviane","Pedro"];
+const notas = [10, 8, 9, 3, 4, 2];
 
 const reprovados = alunos.filter((aluno, indice) => notas[indice] < 5);
 console.log(reprovados)

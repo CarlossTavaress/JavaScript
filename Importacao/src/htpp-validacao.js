b3b9dee87399609bbdf57f841042c312475b90bc
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-11-11 19:18:50 -0300

    usando o fetch para verificar os links

--- Importacao/src/htpp-validacao.js
+++ Importacao/src/htpp-validacao.js
@@ -2,7 +2,18 @@ function extraiLinks (arrLinks){
     return arrLinks.map((objetoLink) => Object.values(objetoLink).join())
 }
 
+async function checaStatus(listUrls){
+    return  listUrls.map(async(url) => {
+        const response = await fetch(url)
+        return response.status;
+    })
+}
+
 
 export default function validaLinks(listaDeLinks){
-    return extraiLinks(listaDeLinks);
+    const links =  extraiLinks(listaDeLinks);
+    const status = checaStatus(links);
+    console.log(status);
+    return status;
+
 }

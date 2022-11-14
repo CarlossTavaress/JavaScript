0c013a57d13458c9956c50254252e0f9068cc592
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-11-14 18:42:39 -0300

    exemplo de classe

---
+++ OrientacaoObjetos/user.js
@@ -0,0 +1,12 @@
+const aluno = {
+    nome: stringNome,
+    nascimento: stringData,
+    turma: stringTurma,
+    nota: notaAluno,
+    status: {
+        // false = reprovado , true = aprovado
+        geografia: boolAproved,
+        matemática: boolAproved,
+        física: boolAproved,
+    }
+}

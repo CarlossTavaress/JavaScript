bb483f88d59ecb23deefe1910b9b8b63a59634f2
Carlos Tavares <111020017+CarlossTavaress@users.noreply.github.com>
2022-11-18 19:57:19 -0300

    arrumando o projeto

--- OrientacaoObjetos/Userr.js
+++ OrientacaoObjetos/Userr.js
@@ -1,4 +1,4 @@
-class User {
+export default class User {
     constructor(nome, email, nascimento, role, ativo = true){
         this.nome = nome;
         this.email = email;
@@ -12,4 +12,3 @@ class User {
     }
 }
 
-const novoUser = new User('Doutor do Amor', 'doutorlove@contato.com', '03/06/1997', 'admin', true);
